62b348fad46d1cbfb43d58a22aa68dd3b5d7173f
fylfot <mr.fylfot@gmail.com>
2012-02-18 16:37:20 +0200

    Non informative reports was added

---
+++ Stone/views/ReportPeriodView.h
@@ -0,0 +1,13 @@
+//
+//  ReportPeriodView.h
+//  Stone
+//
+//  Created by Stanislav Cherednichenko on 18.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ReportPeriodView : NSView
+
+@end
---
+++ Stone/views/ReportPeriodView.m
@@ -0,0 +1,28 @@
+//
+//  ReportPeriodView.m
+//  Stone
+//
+//  Created by Stanislav Cherednichenko on 18.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReportPeriodView.h"
+
+@implementation ReportPeriodView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
